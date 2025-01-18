1e93079a86d052fd0e60815220bcdc7493449a45
shivam-2008 <shivamyadav142311@gmail.com>
2025-01-18 10:43:41 +0530

    Add files via upload
    Signed-off-by: shivam-2008 <shivamyadav142311@gmail.com>

--- waypoint.js
+++ waypoint.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
                 element.addClass('animated fadeInDownBig');
                 element.removeClass('ain');
             },
-            offset: '850'
+            offset: '650'
         });
     });
     $('.t2').each(function () {
@@ -25,10 +25,10 @@ $(document).ready(function () {
         new Waypoint({
             element: element[0],
             handler: function (direction) {
-                element.addClass('animated bounceInRight');
+                element.addClass('animated fadeInRightBig');
                 element.removeClass('ain');
             },
-            offset: '850'
+            offset: '550'
         });
     });
     $('.btn1').each(function () {
@@ -38,7 +38,92 @@ $(document).ready(function () {
             handler: function () {
                 element.addClass('fadeInLeftBig');
                 element.removeClass('ain');
-            }, offset: '850'
+            }, offset: '680'
+        });
+    });
+    $('.btn2').each(function () {
+        var element = $(this);
+        new Waypoint({
+            element: element[0],
+            handler: function () {
+                element.addClass('animated zoomIn');
+                element.removeClass('ain');
+            }, offset: '300'
         });
     });
 });
+
+
+
+$(document).ready(function () {
+    let cards = $('.cards');
+    let stackArea = $('.stack-area');
+
+    function rotateCards() {
+        let angle = 0;
+        let index = 0;
+        cards.each(function () {
+            $(this).css('transform', `rotate(${angle}deg)`);
+            angle -= 10;
+        });
+        cards.each(function (i) {
+            if ($(this).hasClass('away')) {
+                $(this).css('transform', 'translateY(-120vh) rotate(-48deg)');
+            } else {
+                $(this).css('z-index', cards.length - index);
+                index += 1;
+            }
+        });
+    }
+
+    $(window).on('scroll', function () {
+        let distance = $(window).height() / 2;
+        let topVal = stackArea[0].getBoundingClientRect().top;
+        let index = -1 * (topVal / distance + 1);
+        index = Math.floor(index);
+        cards.each(function (i) {
+            console.warn("i : "+i)
+            console.warn("index : "+i)
+            if (i <= index) {
+                $(this).addClass('away');
+            } else {
+                $(this).removeClass('away');
+            }
+        });
+        rotateCards();
+    });
+});
+
+
+// var card = cards.length;
+// console.log(card);
+// let angle = 0;
+// let index = 0;
+// function rotateCards() {
+//     for (let i = 0; i < cards.length; i++) {
+//         if (cards[i].classList.contains("away")) {
+//             cards[i].style.transform = `translateY(-120vh) rotate(-48deg)`
+//         }
+//         else {
+//             cards[i].style.zIndex = cards.length - index;
+//             index += 1;
+//         }
+//     }
+// }
+
+// let stackArea = document.querySelector('.stack-area');
+// window.addEventListener("scroll", () => {
+//     let distance = window.innerHeight / 2;
+//     let topVal = stackArea.getBoundingClientRect().top;
+//     let index = -1 * (topVal / distance + 1);
+//     index = Math.floor(index);
+//     for (let i = 0; i < cards.length; i++) {
+//         if (i <= index) {
+//             cards[i].classList.add("away");
+//         }
+//         else {
+//             cards[i].classList.remove("away");
+//         }
+//     }
+//     rotateCards();
+// })
